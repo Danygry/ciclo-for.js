a498a82bfde4117d2bbdefa976018e647db14dad
Daniele Grilli <81428089+Danygry@users.noreply.github.com>
2022-02-03 15:11:04 +0100

    Create for.js

---
+++ for.js
@@ -0,0 +1,22 @@
+//ciclo for
+
+  for (i = 0; i < 25; i++) { //incremento
+    console.log(i);
+  }
+
+for(i = 40; i > 0; i--){  //decremento
+  console. log(i);
+}
+
+let listaForme = ["quadrato", "cerchio", "rettangolo", "triangolo"];
+
+for(i=0; i<listaForme.length; i++){
+  console.log(listaForme[i]);
+}
+
+
+
+let numeri = [1, 14, 29, 50];
+for(i= 0; i<numeri.length; i++){
+  console.log(numeri[i]);
+}
